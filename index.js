bf39548303c3a1f91e0e16716c07d5399bb7034a
Vinay <vinay.unision@gmail.com>
2022-09-04 18:53:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+
+for(var i=0; i<7; i++){
+    document.querySelectorAll("button")[i].addEventListener("click", handleClick);    
+}
+function handleClick(){
+    var buttonInner=this.innerHTML;
+    makeSound(buttonInner);
+    buttonKey(buttonInner);
+}
+document.addEventListener("keydown", function(event){
+    makeSound(event.key);
+    buttonKey(event.key);
+})
+function makeSound(key){
+    switch (key){
+        case 'w':
+            var audio = new Audio("tom-1.mp3");
+            audio.play();
+        break;  
+        case 'a':
+            var audio = new Audio("tom-2.mp3");
+            audio.play();
+        break;  
+        case 's':
+            var audio = new Audio("tom-3.mp3");
+            audio.play();
+        break;  
+        case 'd':
+            var audio = new Audio("tom-4.mp3");
+            audio.play();
+        break;  
+        case 'j':
+            var audio = new Audio("crash.mp3");
+            audio.play();
+        break;  
+        case 'k':
+            var audio = new Audio("kick-bass.mp3");
+            audio.play();
+        break; 
+        case 'l':
+            var audio = new Audio("snare.mp3");
+            audio.play();
+        break;         
+    }   
+}
+function buttonKey(k){
+    var activeButton = document.querySelector("."+k);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },100);
+}
